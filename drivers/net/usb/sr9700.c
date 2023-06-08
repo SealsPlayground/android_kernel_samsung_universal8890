dfce5ad823a4ab76917e9a19e471bbad24020ab2
ivanmeler <i_ivan@windowslive.com>
2023-06-08 16:41:43 +0000

    ethernet: add 0fe6:9702 support
    its same as 9700 but with storage for windows drivers built in

--- drivers/net/usb/sr9700.c
+++ drivers/net/usb/sr9700.c
@@ -537,6 +537,10 @@ static const struct usb_device_id products[] = {
 		USB_DEVICE(0x0fe6, 0x9700),	/* SR9700 device */
 		.driver_info = (unsigned long)&sr9700_driver_info,
 	},
+        {
+                USB_DEVICE(0x0fe6, 0x9702),      /* DM9601 USB to Fast Ethernet Adapter */
+                .driver_info = (unsigned long)&sr9700_driver_info,
+        },
 	{},			/* END */
 };
 
